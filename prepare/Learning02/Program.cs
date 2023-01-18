d82830a3563db6176b6d85d7060a02dc542421d0
Trentbn <trentnelson97@gmail.com>
2023-01-18 15:47:19 -0700

    Completed Abstraction learning activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Apple";
+        job1._startYear = 2022;
+        job1._endYear = 2023;
+
+        job1.Display();
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Engineer";
+        job2._company = "Sony";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        job2.Display();
+
+        Resume resume = new Resume();
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        Console.WriteLine($"Company of job1: {resume._jobs[0]._company}");
+        resume.Display();
     }
 }
