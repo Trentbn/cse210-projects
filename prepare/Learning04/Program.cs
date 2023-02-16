5b126505a94dd7a31bb0e536a73e4b652aa9417e
Trentbn <trentnelson97@gmail.com>
2023-02-16 12:38:34 -0700

    Completed Inheritance Learning Activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Trent Nelson", "CSE210");
+
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Taylor Nelson", "Calculas", "4.3", "5-10");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Willie Nelson", "English", "Why Skateboarding is Cool");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInfo());
     }
 }
