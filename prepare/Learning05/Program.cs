9d35d9fb0e1344bb0258e7a2a9ef77b25a233f57
Trentbn <trentnelson97@gmail.com>
2023-03-02 11:42:57 -0700

    Completed shapes activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("Blue", 5);
+        //DisplayShapes(square);
+
+        Rectangle rectangle = new Rectangle("Red", 5, 10);
+        //DisplayShapes(rectangle);
+
+        Circle circle = new Circle("Yellow", 5);
+        //DisplayShapes(circle);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach(Shape shape in shapes)
+        {
+            DisplayShapes(shape);
+        }
+
     }
+
+    public static void DisplayShapes(Shape shape)
+    {
+        Console.WriteLine(shape.GetColor());
+        Console.WriteLine(shape.GetArea());
+    }
+
 }
